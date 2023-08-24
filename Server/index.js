5bfd20463df1ea177d6477eb13a4d99a48e9a579
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-08-25 00:13:07 +0530

    minr

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,51 @@
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io'); // Use the Server class
+// const cors = require('cors');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'http://localhost:4200', // Your Angular app's domain
+//     methods: ['GET', 'POST'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
+//   },
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   socket.on('drawing', (data) => {
+//     socket.broadcast.emit('drawing', data);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io'); // Use the Server class
-const cors = require('cors');
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:4200', // Your Angular app's domain
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  },
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running...');
 });
 
-const PORT = process.env.PORT || 3000;
+const io = new Server(server);
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -26,8 +57,4 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
