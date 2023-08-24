78aef87ab4c042efdacaa0036300ba92fd0f5798
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-08-24 19:26:31 +0530

    Minor changes

---
+++ Server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io'); // Use the Server class
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:4200', // Your Angular app's domain
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  },
+});
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('drawing', (data) => {
+    socket.broadcast.emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
