f57fcdf53a66edf07e625c91793bae79457dea91
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-08-25 00:35:16 +0530

    ss

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,7 @@ const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  },
+  cors: cors()
 });
 
 const PORT = process.env.PORT || 3000;
